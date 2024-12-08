d23a07069535ee04b177f71c78d8c360c5dae581
Ashikawa <ashikawaharuki@MUMac.local>
2024-12-08 21:56:41 +0900

    [ドキュメントの更新] datafact, データ操作フローのサンプルを保存

---
+++ data/samples/datafact.py
@@ -0,0 +1,54 @@
+import sys
+sys.path.append('/Users/ashikawaharuki/Desktop/research/TDB/test/system/src')
+# 自分で作成したクラス、関数をimport
+from datafact_model import Datafact
+
+
+ordinal_d = {"年":['2024', '2023', '2022', '2021', '2020', '2019', '2018']}
+
+
+datafact1 = Datafact(
+    subject = [{"大分類":"製造業","都道府県":"静岡"},"年",["2022"]],
+    operation = [
+        "ScalarArithmetic",
+        "-",
+        Datafact(
+            subject = [{"大分類":"製造業","年":"2022"},"都道府県",["静岡"]],
+            operation = [
+                "Rank",
+                "降順",
+                Datafact(
+                    subject = [{"大分類":"製造業","年":"2022"},"都道府県",["*"]],
+                    operation = ["Aggregation", "売上", "sum"]
+                )
+            ]
+        ),
+        Datafact(
+            subject = [{"大分類":"製造業","年":"2021"},"都道府県",["静岡"]],
+            operation = [
+                "Rank",
+                "降順",
+                Datafact(
+                    subject = [{"大分類":"製造業","年":"2021"},"都道府県",["*"]],
+                    operation = ["Aggregation", "売上", "sum"]
+                )
+            ]
+        )
+    ],
+)
+
+datafact2 = Datafact(
+    subject=[{"大分類":"サービス業"}, "年", ["2022"]],
+    operation=[
+        "ScalarArithmetic", 
+        "/",
+        Datafact(
+            subject=[{"大分類":"サービス業"}, "年", ["2022"]],
+            operation=["Aggregation", "売上", "sum_percent"]
+        ),
+        Datafact(
+            subject=[{"大分類":"サービス業"}, "年", ["2021"]],
+            operation=["Aggregation", "売上", "sum_percent"]
+        )
+    ]
+)
