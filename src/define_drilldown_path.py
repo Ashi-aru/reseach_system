e9c05cd8b2ad5dfb515f0bb76ae31bd1303e2174
Ashikawa <ashikawa.h.aa@m.titech.ac.jp>
2024-12-13 10:08:59 +0900

    [ドキュメントの更新] 出力例・入力例のコメント追加

--- src/define_drilldown_path.py
+++ src/define_drilldown_path.py
@@ -36,11 +36,47 @@ def make_sample_data(sample_df, attribute_type):
     return sample_data
 
 # ドリルダウンの案を列挙する
-# 入力：{"description":表データの説明, "analysis_goal":データ分析の目標, "sample_data":サンプルデータ}
-# サンプルデータは上のmake_sample_dataで生成する
+"""
+【入力例】
+"sample_data"は上のmake_sample_dataで生成する
+{
+    "description":"各企業の各年における財務状況データ",
+    "analysis_goal":"業種ごとの財務パフォーマンスの特徴を明らかにする",
+    "sample_data":{
+        "年":{
+            "values":[2020,2021],
+            "attribute_type":"Ordinal"
+        },
+        "企業名":{
+            "values":["株式会社A","株式会社B"],
+            "attribute_type":"Categorical"
+        },
+        "業種":{
+            "values":["製造業","サービス業"],
+            "attribute_type":"Categorical"
+        },
+        "従業員数":{
+            "values":["500~1000","0~500"],
+            "attribute_type":"Ordinal"
+        },
+        "売上高(百万円)":{
+            "values":[5692,7813],
+            "attribute_type":"Quantitative"
+        },
+        "純利益(百万円)":{
+            "values":[352,152],
+            "attribute_type":"Quantitative"
+        },
+        "ROE(%)":{
+            "values":[9.3529453168714,8.51822829446703],
+            "attribute_type":"Quantitative"
+        }
+    }
+}
+"""
 def list_drilldown_ideas(input_data):
     API_KEY = os.getenv('OPENAI_API_KEY')
-    MODEL = "o1-preview" # gpt-4o-mini, gpt-4o-2024-05-13, gpt-4o-2024-08-06, o1-preview
+    MODEL = "gpt-4o-2024-08-06" # gpt-4o-mini, gpt-4o-2024-05-13, gpt-4o-2024-08-06, o1-preview
     with open(PROMPT_DIR/"define_drilldown_path.txt", "r") as f:  
         BASE_INSTRUCTION = f.read()
 
@@ -52,7 +88,8 @@ def list_drilldown_ideas(input_data):
     response = client.chat.completions.create(model=MODEL, messages=messages)
     content = response.choices[0].message.content
     response = to_dict_recursive(response)
-    logging.info(content)
+    # logging.info(content)
+    logging.info(json.dumps(response,ensure_ascii=False,indent=4))
     return [content, response]
 
 
@@ -76,13 +113,36 @@ def check_drilldown_ideas(drilldown_ideas, main_df, attribute_type):
         for column_name in drilldown["drilldown"]:
             node_n *= unique_num_d[column_name]
         drilldown_ideas[idea_n]["flg"] = (node_n<10**6)
-    logging.info(drilldown_ideas)
+    logging.info(json.dumps(drilldown_ideas,ensure_ascii=False,indent=4))
     return drilldown_ideas
 
 
 # ----------------------------------------------------------------
 # ③ ①,②のまとめ
-
+"""
+【出力例】
+{
+    "drilldown1": {
+        "reasoning": "まずは、ユーザーが居住する州ごとに購入パターンを確認することで地理的な偏りや特徴を理解することができます。",
+        "drilldown": [
+            "Shipping Address State"
+        ],
+        "expected_report": "各州におけるAmazonユーザーの購入パターンを可視化し、州ごとに人気のあるカテゴリーや商品が把握できる。",
+        "flg": true
+    },
+    "drilldown2": {
+        "reasoning": "異なる地域での異なる商品カテゴリの購入傾向を理解するために、州ごとに商品のカテゴリに分けて分析します。",
+        "drilldown": [
+            "Shipping Address State",
+            "Category"
+        ],
+        "expected_report": "州ごとの購入傾向から、特定のカテゴリが地域によってどのように変わるかを分析します。",
+        "flg": true
+    },
+    ...
+    }
+}
+"""
 def define_drilldown_path(main_df, sample_df, description, analysis_goal, attribute_type):
     # ①
     sample_data = make_sample_data(sample_df=sample_df, attribute_type=attribute_type)
@@ -95,7 +155,7 @@ def define_drilldown_path(main_df, sample_df, description, analysis_goal, attrib
 
 
 if __name__ == "__main__":
-    main_df = pd.read_csv(DATA_DIR/'amazon-purchases.csv')
+    main_df = pd.read_csv(DATA_DIR/'tables/_amazon-purchases.csv')
     sample_df = main_df.head(2)
     description = "2018年から2022年にかけての米国の5027人のAmazon.comユーザーの購入履歴。データセットのサイズは300MB超。"
     analysis_goal = "地理的な購買パターンの分析"
