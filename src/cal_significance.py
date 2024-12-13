c669c43b457085fec5233077f6f6a35b2878b982
Ashikawa <ashikawa.h.aa@m.titech.ac.jp>
2024-12-13 16:03:30 +0900

    [テスト追加・バグの修正] 外れ値の検出についてバグを修正
    - p値の計算を修正。
    - t_valueの計算式は正しかった。
    - ルートの中身が負になる時の対応を追加

--- src/cal_significance.py
+++ src/cal_significance.py
@@ -18,20 +18,20 @@ logging.basicConfig(
 
 """
 Grubbs検定で外れ値を検出し、そのp値を計算する関数。
-以下の2ステップを繰り返す（[外れ値が検出されなくなる or valuesの長さが3未満になる]まで）
-1. 最大値方向の外れ値検出→最大値を除去
-2. 最小値方向の外れ値検出→最小値を除去
-
+以下の3ステップを繰り返す（[外れ値が検出されなくなる or valuesの長さが3未満になる]まで）
+1. 平均から最も外れた値を選出
+2. 外れ値の判定を行う
+3. 外れ値の場合、p値を保存→この値をデータ群から除外
 
 入力
 - data: 数値のリスト
     - {"製造業":100, "サービス業":500,...,"その他":150}
 - alpha:有意水準
 出力
-- max方向の外れ値のリスト ["サービス業",..]
-- min方向の外れ値のリスト ["製造業",...]
+- 外れ値の辞書
+    - {"サービス業":{"p_value":0.002, "data_value":150},..}
 """
-def detect_outliers_with_p_values_both_directions(data, alpha=0.05):
+def detect_outliers(data, alpha=0.05):
     """
     outliersライブラリを用いて複数の外れ値を最大値方向と最小値方向に対して検出し、
     p値を計算して出力する。
@@ -47,8 +47,17 @@ def detect_outliers_with_p_values_both_directions(data, alpha=0.05):
         suspect_value, suspect_name = values[max_diff_index], keys[max_diff_index]
 
         G = abs(suspect_value - mean) / std
-        t_value = math.sqrt(n*(n-2)*(G**2) / ((n-1)**2 - n*(G**2))) # ((n-1)**2 * G**2) / (n - (n-1)*
-        p_value = (1 - t.cdf(t_value, n-2))*(2*n)
+        if((n-1)**2 - n*(G**2)>0):
+            t_value = math.sqrt(n*(n-2)*(G**2) / ((n-1)**2 - n*(G**2)))
+            p_value = (1 - t.cdf(t_value, n-2))*(2*n)
+        else:
+            """
+            t_valueのルートの中身が負になる場合はp値を0にする。
+            G > (n-1)/sqrt(n)の時に、ルートの中身が0になる。
+            G = abs(x-mean)/stdより、abs(x-mean)が大きすぎるとルートの中身が0になる
+            → めちゃくちゃ外れ値ということなので、p値は0でいいのではないでしょうか？
+            """
+            p_value = 0  # 計算できない場合は0を代入
         
         if(p_value <= alpha):
             del data[suspect_name]
@@ -56,9 +65,3 @@ def detect_outliers_with_p_values_both_directions(data, alpha=0.05):
         else:
             break  
     return result_d
-
-# 使用例
-values = dict([(str(i),n) for i, n in enumerate([10, 12, 15, 18, 100, 102, 120, -20])])
-alpha = 0.05
-
-
