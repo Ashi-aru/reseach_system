5934d371e6a762ca3bc7bb786c5f690a4e715970
Ashikawa <ashikawa.h.aa@m.titech.ac.jp>
2024-12-13 10:10:36 +0900

    [バグ修正] to_dict_recursiveについて、モデルによってバグが出る箇所をコメントアウト

--- src/others.py
+++ src/others.py
@@ -13,9 +13,6 @@ logging.basicConfig(
         format='%(asctime)s\n%(message)s'
     )
 
-TODAY = date.today().strftime("%Y-%m-%d")
-
-
 # responseはChatCompletionオブジェクトで、これをリストに格納してしまうと、jsonとして保存ができないので、
 # ChatCompletionオブジェクトを再帰的に計算し、ネストした辞書に変換する関数を定義
 def to_dict_recursive(chat_completion_object):
@@ -40,9 +37,9 @@ def to_dict_recursive(chat_completion_object):
             "prompt_tokens": chat_completion_object.usage.prompt_tokens,
             "completion_tokens": chat_completion_object.usage.completion_tokens,
             "total_tokens": chat_completion_object.usage.total_tokens,
-            "completion_tokens_details": {
-                "reasoning_tokens": chat_completion_object.usage.completion_tokens_details.reasoning_tokens,
-            }
+            # "completion_tokens_details": {
+            #     "reasoning_tokens": chat_completion_object.usage.completion_tokens_details.reasoning_tokens,
+            # }
         },
         "time": TODAY
     }
