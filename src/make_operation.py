3fd820d690a25d7898aecb8599beecf770f7972c
Ashikawa <ashikawa.h.aa@m.titech.ac.jp>
2024-12-14 09:06:20 +0900

    [リファクタリング・ドキュメント更新] 入力/出力説明の修正。ScalarArithmetic用演算子の決定について仕様変更。
    ScalarArithmeticは時系列ノードでのみ実施する仕様に変更。

--- src/make_operation.py
+++ src/make_operation.py
@@ -38,8 +38,11 @@ OPERATOR_D = {
 """
 注目属性ごとにAggregation関数を選択する処理の実装
 
-入力: (att_l, att_label_d)=(["売上","従業員数","上場flg"],{"売上":"Quantitative","上場flg":"Categorical",...})
-出力: {"売上":[1,3,5],"従業員数":[1,3,5],"上場flg":[2]}
+入力
+- att_l: ["売上","従業員数","上場flg"]
+- att_label_d: {"売上":"Quantitative","上場flg":"Categorical",...})
+出力
+- {"売上":[1,3,5],"従業員数":[1,3,5],"上場flg":[2]}
 """
 def define_aggregation_F(att_l, att_label_d):
     return_d = {}
@@ -56,19 +59,21 @@ def define_aggregation_F(att_l, att_label_d):
 
 
 """
-注目属性, Aggregation関数, ドリダウン属性の組み合わせごとにScalar Arithmeticの演算子を決定する処理の実装
+注目属性の属性タイプ(CA,QA,OA)・Aggregation関数の組み合わせごとにScalarArithmeticの演算子を決定する処理の実装
 
-入力: (focus_att_l,focus_att2func_d,drilldown_att_l)
-    - focus_att_l: 注目属性(計算を行う属性)のリスト, ["売上","従業員数"]
-    - focus_att2func_d: define_aggregation_Fの出力, {"売上":[1,3,4],"従業員数":[1,3,4],"上場flg":[9]}
-出力: {(注目属性, Aggregation_F):演算子, ...}
+入力:
+- focus_att_l: 注目属性(計算を行う属性)のリスト, ["売上","従業員数"]
+- focus_att2func_d: define_aggregation_Fの出力, {"売上":[1,3,4],"従業員数":[1,3,4],"上場flg":[9]}
+- attr_type_d: 各属性のタイプ(Categoricalなど)を保存した辞書
+出力: 
+- {(注目属性, Aggregation_F):演算子, ...}
 """
-def define_scalar_arithmetic_operator(focus_att_l, focus_att2func_d):
+def define_scalar_arithmetic_operator(focus_att_l, focus_att2func_d, attr_type_d):
     return_d = {}
     for focus_att in focus_att_l:
         for func_num in focus_att2func_d[focus_att]:
-            if(func_num<10):
-                return_d[(focus_att,func_num)]=[3,4] # 数字の意味についてはOPERATOR_Dを参照
+            if(func_num<10 and attr_type_d[focus_att]=="Ordinal_t"): # 時系列データにのみSclarArithmeticを実施
+                return_d[(focus_att,func_num)]=[2,4] # 数字の意味についてはOPERATOR_Dを参照
             else:
                 return_d[(focus_att,func_num)]=[None]
     return return_d
