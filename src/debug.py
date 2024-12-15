ebd4ee686c02ac44eb6df76c8b311ab84ed42563
Ashikawa <ashikawa.h.aa@m.titech.ac.jp>
2024-12-15 15:52:54 +0900

    [機能追加・テスト追加・バグ修正・リファクタリング] operationのdebugを行う関数を追加。datafactの言語化関数について仕様変更
    - debug_operation: operationのdebugを実施
    - unpack_datafactをdebug_datafactの外に移動（debug_operationでも使用するため。）

--- src/debug.py
+++ src/debug.py
@@ -9,6 +9,65 @@ DATA_DIR = PROJ_DIR/"data"
 TODAY = date.today().strftime("%Y-%m-%d")
 
 logger = setup_logger()
+
+
+"""
+データファクトをパーツごとに分解し、文字列としてリストに格納する関数
+入力
+- datafact: Datafactオブジェクト
+出力
+- datafactを文字列にしたもの（\t,\nを含む形で）
+"""
+def unpack_datafact(datafact):
+    part_l = []
+    parents, column_name, filter_values = datafact.subject
+    operation_name, *operation_others = datafact.operation
+    if(operation_name=="Aggregation"):
+        part_l.append(f"Datafact(subject={datafact.subject},operation={datafact.operation}),")
+        return part_l
+    elif(operation_name=="ScalarArithmetic"):
+        op, datafact1, datafact2 = operation_others
+        l = ["Datafact(", f"subject={datafact.subject},", "operation=[", "ScalarArithmetic,", f"{op},"]+unpack_datafact(datafact1)+unpack_datafact(datafact2)+["]", ")"]
+        for part in l:
+            part_l.append(part)
+        return part_l
+    elif(operation_name=="Rank"):
+        order, datafacts = operation_others
+        l = ["Datafact(", f"subject={datafact.subject},", "operation=[", "Rank,", f"{order},"]+unpack_datafact(datafacts)+["]", ")"]
+        for part in l:
+            part_l.append(part)
+        return part_l
+    else:
+        raise ValueError("Unknown operation")
+
+"""
+operationをパーツごとに分解し、文字列としてリストに格納する関数
+入力
+- operation: Datafact.operation
+出力
+- operationを文字列にしたもの（\t,\nを含む形で）
+"""
+def unpack_operation(operation):
+        part_l = []
+        operation_name, *operation_others = operation
+        if(operation_name=="Aggregation"):
+            part_l.append(f",operation={operation}),")
+            return part_l
+        elif(operation_name=="ScalarArithmetic"):
+            op, datafact1, datafact2 = operation_others
+            l = ["operation=[", "ScalarArithmetic,", f"{op},"]+unpack_datafact(datafact1)+unpack_datafact(datafact2)+["]"]
+            for part in l:
+                part_l.append(part)
+            return part_l
+        elif(operation_name=="Rank"):
+            order, datafacts = operation_others
+            l = ["operation=[", "Rank,", f"{order},"]+unpack_datafact(datafacts)+["]"]
+            for part in l:
+                part_l.append(part)
+            return part_l
+        else:
+            raise ValueError("Unknown operation")
+
 """
 datafactをデバックする関数。
 普通にデバックすると、['ScalarArithmetic', '-', <datafact_model.Datafact object at 0x10615fac0>, <datafact_model.Datafact object at 0x10615f280>]となってしまい、中身が確認できない。
@@ -16,31 +75,30 @@ datafactをデバックする関数。
 入力:
 - datafact
 出力
-- string or リスト
+- string
 """
 def debug_datafact(datafact):
-    def unpack_datafact(datafact):
-        part_l = []
-        parents, column_name, filter_values = datafact.subject
-        operation_name, *operation_others = datafact.operation
-        if(operation_name=="Aggregation"):
-            part_l.append(f"Datafact(subject={datafact.subject},operation={datafact.operation}),")
-            return part_l
-        elif(operation_name=="ScalarArithmetic"):
-            op, datafact1, datafact2 = operation_others
-            l = ["Datafact(", f"subject={datafact.subject},", "operation=[", "ScalarArithmetic,", f"{op},"]+unpack_datafact(datafact1)+unpack_datafact(datafact2)+["]", ")"]
-            for part in l:
-                part_l.append(part)
-            return part_l
-        elif(operation_name=="Rank"):
-            order, datafacts = operation_others
-            l = ["Datafact(", f"subject={datafact.subject},", "operation=[", "Rank,", f"{order},"]+unpack_datafact(datafacts)+["]", ")"]
-            for part in l:
-                part_l.append(part)
-            return part_l
-        else:
-            raise ValueError("Unknown operation")
     part_l = unpack_datafact(datafact)
+    all_str, tab_n = "", 0
+    for part in part_l:
+        if(part in [")", "]"]):
+            tab_n -= 1
+        all_str += ("\t"*tab_n + part + "\n")
+        if(part in ["Datafact(","operation=["]):
+            tab_n += 1
+    return all_str
+
+"""
+operationをデバックする関数。
+普通にデバックすると、['ScalarArithmetic', '-', <datafact_model.Datafact object at 0x10615fac0>, <datafact_model.Datafact object at 0x10615f280>]となってしまい、中身が確認できない。
+→ 中身を確認できるようにする
+入力:
+- datafact.operation
+出力
+- string
+"""
+def debug_operation(operation):
+    part_l = unpack_operation(operation)
     all_str, tab_n = "", 0
     for part in part_l:
         if(part in [")", "]"]):
@@ -82,3 +140,4 @@ if __name__ == "__main__":
         ]
     )
     logger.info(debug_datafact(datafact))
+    logger.info(debug_operation(datafact.operation))
