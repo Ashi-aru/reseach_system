af744776a03247719afe61a1bac3faccd4a6fa71
Ashikawa <ashikawa.h.aa@m.titech.ac.jp>
2024-12-15 15:35:56 +0900

    [機能追加・テスト追加・バグ修正] datafactのデバッグ(datafactを文字列として出力)する関数の実装
    - debug_datafact

---
+++ src/debug.py
@@ -0,0 +1,84 @@
+from pathlib import Path
+from datetime import date
+# 自分で定義した関数・クラスをimport
+from datafact_model import Datafact
+from logging_config import setup_logger
+
+PROJ_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = PROJ_DIR/"data"
+TODAY = date.today().strftime("%Y-%m-%d")
+
+logger = setup_logger()
+"""
+datafactをデバックする関数。
+普通にデバックすると、['ScalarArithmetic', '-', <datafact_model.Datafact object at 0x10615fac0>, <datafact_model.Datafact object at 0x10615f280>]となってしまい、中身が確認できない。
+→ 中身を確認できるようにする
+入力:
+- datafact
+出力
+- string or リスト
+"""
+def debug_datafact(datafact):
+    def unpack_datafact(datafact):
+        part_l = []
+        parents, column_name, filter_values = datafact.subject
+        operation_name, *operation_others = datafact.operation
+        if(operation_name=="Aggregation"):
+            part_l.append(f"Datafact(subject={datafact.subject},operation={datafact.operation}),")
+            return part_l
+        elif(operation_name=="ScalarArithmetic"):
+            op, datafact1, datafact2 = operation_others
+            l = ["Datafact(", f"subject={datafact.subject},", "operation=[", "ScalarArithmetic,", f"{op},"]+unpack_datafact(datafact1)+unpack_datafact(datafact2)+["]", ")"]
+            for part in l:
+                part_l.append(part)
+            return part_l
+        elif(operation_name=="Rank"):
+            order, datafacts = operation_others
+            l = ["Datafact(", f"subject={datafact.subject},", "operation=[", "Rank,", f"{order},"]+unpack_datafact(datafacts)+["]", ")"]
+            for part in l:
+                part_l.append(part)
+            return part_l
+        else:
+            raise ValueError("Unknown operation")
+    part_l = unpack_datafact(datafact)
+    all_str, tab_n = "", 0
+    for part in part_l:
+        if(part in [")", "]"]):
+            tab_n -= 1
+        all_str += ("\t"*tab_n + part + "\n")
+        if(part in ["Datafact(","operation=["]):
+            tab_n += 1
+    return all_str
+
+
+if __name__ == "__main__":
+    datafact = Datafact(
+        subject=[{"Shipping Address State":"MA"}, "y", ["*"]],
+        operation=[
+            "ScalarArithmetic", 
+            "-",
+            Datafact(
+                subject=[{"Shipping Address State":"MA"},"y",["n"]],
+                operation=[
+                    "Rank",
+                    "降順",
+                    Datafact(
+                        subject=[{"Shipping Address State":"MA"},"y",["*"]],
+                        operation=["Aggregation", "Purchase Price Per Unit", "mean"]
+                    )
+                ]
+            ),
+            Datafact(
+                subject=[{"Shipping Address State":"MA"},"y",["n-1"]],
+                operation=[
+                    "Rank",
+                    "降順",
+                    Datafact(
+                        subject=[{"Shipping Address State":"MA"},"y",["*"]],
+                        operation=["Aggregation", "Purchase Price Per Unit", "mean"]
+                    )
+                ]
+            ),
+        ]
+    )
+    logger.info(debug_datafact(datafact))
